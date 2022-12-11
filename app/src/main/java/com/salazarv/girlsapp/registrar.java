dbb446aebfecad75d19c556554ffa6a1187214c2
WilliansSamaelPantojaAntay <willianssamael@gmail.com>
2022-12-11 18:31:57 -0500

    Actualización 2 Willians

--- app/src/main/java/com/salazarv/girlsapp/registrar.java
+++ app/src/main/java/com/salazarv/girlsapp/registrar.java
@@ -1,29 +1,87 @@
 package com.salazarv.girlsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class registrar extends AppCompatActivity {
 
+    EditText txtNombre,txtApellidos,txtTelefono,txtCorreo,txtFecNac,txtpass1,txtpass2;
+    Button btnRegistrar;
 
-    EditText fec_nac;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
 
-        fec_nac = findViewById(R.id.txtFecNac);
-        fec_nac.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellidos = findViewById(R.id.txtApellidos);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtFecNac = findViewById(R.id.txtFecNac);
+        txtpass1 = findViewById(R.id.txtpass1);
+        txtpass2 = findViewById(R.id.txtpass2);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        txtFecNac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CalendarView.generateViewId();
             }
         });
 
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrar();
+            }
+        });
+
+    }
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registrar(){
+        if(txtpass1.getText().toString().equals(txtpass2.getText().toString())){
+            mAuth.createUserWithEmailAndPassword(txtCorreo.getText().toString(),txtpass1.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(registrar.this, "Usuario creado", Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent= new Intent(registrar.this,MainActivity.class);
+                                startActivity(intent);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(registrar.this, "Falló la autenticación", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
     }
 }
