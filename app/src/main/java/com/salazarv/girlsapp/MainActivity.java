e776f95d5aba5b935a7acf5ad04b865084810d70
Ervinsalva97 <77037376+Ervinsalva97@users.noreply.github.com>
2022-12-11 19:24:51 -0500

    se agrega validación de btn_Is

--- app/src/main/java/com/salazarv/girlsapp/MainActivity.java
+++ app/src/main/java/com/salazarv/girlsapp/MainActivity.java
@@ -1,18 +1,33 @@
 package com.salazarv.girlsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText txtcorreo, txtpass;
     Button btn_Registrar, btn_Is;
+
+    // Componente de firebase
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     @Override
@@ -20,8 +35,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Instanciamos el componente de firebase Autentification
+        mAuth = FirebaseAuth.getInstance();
+
+        txtcorreo = findViewById(R.id.txtcorreo);
+        txtpass = findViewById(R.id.txtpass);
         btn_Is = findViewById(R.id.btnIs);
-        btn_Registrar =  findViewById(R.id.btnRegistrar);
+        btn_Registrar = findViewById(R.id.btnRegistrar);
+
+
+        //        Evento onClic al boton btn_ingresar
+        btn_Is.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarSesion();
+            }
+        });
+
+        observador();
 
 
         btn_Registrar.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +64,79 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
+
+        private void iniciarSesion() {
+
+            String email = txtcorreo.getText().toString().trim();
+            String clave = txtpass.getText().toString().trim();
+
+//        Validamos que el campo email no esté vacio
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Ingrese su correo electrónico", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+//        Validamos que el campo password no esté vacio
+            if (TextUtils.isEmpty(clave)) {
+                Toast.makeText(this, "Ingrese su contraseña", Toast.LENGTH_SHORT).show();
+                return;
+            }
+//          Mostramos un progressDialog para esperar la confirmación de Firebase
+            ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setTitle("Autentifiacndo");
+            dialog.setMessage("Espere por Favor...");
+            dialog.setCancelable(false);
+            dialog.show();
+
+//        Autenticación al usuario si se encuentra registrado en Firebase en firebase (Autentication)
+
+            mAuth.signInWithEmailAndPassword(email, clave).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Si se ha logeado correctamente", Toast.LENGTH_SHORT).show();
+                    } else {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Usuario y/o contrasñea incorrecta", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        }
+
+
+        private void observador() {
+            mAuthStateListener = firebaseAuth -> {
+                FirebaseUser usuario = firebaseAuth.getCurrentUser();
+                if (usuario == null){
+                    Toast.makeText(this,"No hay sesion activa", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this,"las ession esta activa", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+        }
+
+        @Override
+        protected void onStart(){
+            super.onStart();
+            mAuth.addAuthStateListener(mAuthStateListener);
+        }
+
+        @Override
+        protected void onStop(){
+            super.onStop();
+            if (mAuthStateListener != null){
+                mAuth.removeAuthStateListener(mAuthStateListener);
+            }
+        }
+
+
+
+
 }
