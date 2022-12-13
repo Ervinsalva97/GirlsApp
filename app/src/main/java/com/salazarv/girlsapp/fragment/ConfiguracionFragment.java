ea092efbe0ecca616781fc52917ecb676b7d1de6
Ervinsalva97 <77037376+Ervinsalva97@users.noreply.github.com>
2022-12-12 22:20:17 -0500

    corrección de diseño

--- app/src/main/java/com/salazarv/girlsapp/fragment/ConfiguracionFragment.java
+++ app/src/main/java/com/salazarv/girlsapp/fragment/ConfiguracionFragment.java
@@ -1,65 +1,41 @@
 package com.salazarv.girlsapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-import com.salazarv.girlsapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.salazarv.girlsapp.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConfiguracionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ConfiguracionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FirebaseAuth mAuth;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ConfiguracionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfiguracionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConfiguracionFragment newInstance(String param1, String param2) {
-        ConfiguracionFragment fragment = new ConfiguracionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    Button btnCs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        btnCs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signOut();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false);
-    }
+
+
+
 }
--- app/src/main/java/com/salazarv/girlsapp/fragment/InicioFragment.java
+++ app/src/main/java/com/salazarv/girlsapp/fragment/InicioFragment.java
@@ -7,59 +7,55 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.salazarv.girlsapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InicioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class InicioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ImageSlider carrusel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InicioFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InicioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InicioFragment newInstance(String param1, String param2) {
-        InicioFragment fragment = new InicioFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FirebaseFirestore firestore;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicio, container, false);
+
     }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        firestore = FirebaseFirestore.getInstance();
+//
+//        carrusel = carrusel.findViewById(R.id.textView);
+//
+//        ArrayList<SlideModel> slideModels =  new ArrayList<>();
+//
+//        firestore.collection("Sliders").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if (task.isSuccessful()){
+//                    for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+//                        slideModels.add(new SlideModel(queryDocumentSnapshot.getString("URL"), ScaleTypes.FIT));
+//                        carrusel.setImageList(slideModels, ScaleTypes.FIT);
+//                    }
+//                }else {
+//                    Toast.makeText(getActivity(),"No se ha podido encontrar la tarea",Toast.LENGTH_SHORT).show();
+//                }
+//
+//
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getActivity(), "No se ha encontrado imágenes", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+
+
 }
